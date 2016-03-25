6a6f063a98dfc7c3cdfb49387cad4d713cd2f43d
Alon Robinson <ahrobins3@gmail.com>
2016-03-25 16:11:35 -0700

    initial angularfire generator commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name liberateusApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the liberateusApp
+ */
+angular.module('liberateusApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
